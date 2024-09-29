0c1024453477f90c66f852f0918441877f2029b8
AkashMala <135145391+AkashMala@users.noreply.github.com>
2024-09-29 15:20:50 +0530

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-const  bgImageE1=document.getElementById("bg-image");
+body{
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+    align-items: center;
+    background-color: darkblue;
+    color: white;
+    font-family: cursive;
+    /* text-align: center; */
 
-window.addEventListener("scroll", ()=>{
-    updateImage();
-});
-   
+}
+.container{
+    display: flex;
+    text-align: center;
+  }
+.mouse-event{
+    border: solid 1px lightgray;
+    margin: 40px;
+    min-width: 180px;
+    min-height: 100px;
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    font-size: 30px;
+}
+.mouse-event h4{
+    position: absolute;
+    top: -50px;
+    font-size: 14px;
+    left: 50%;
+    transform: translateX(-50%);
+    white-space: nowrap;
+    font-weight: 100;
+    color: lightgray;
 
+}
 
-function updateImage() {
-    bgImageE1.style.opacity= 1 - window.
-    pageYOffset / 800;
-    bgImageE1.style.backgroundSize=160-
-    window.pageYOffset /12 + "%";
-    console.log(window.pageYOffset ,1-window.pageYOffset / 800);
-    
+@media (max-width:500px){
+    .container{
+        flex-direction: column;
+    }
 }
