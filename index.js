c2388343289b215831fbc6a1e87b5606fb7efaea
AkashMala <135145391+AkashMala@users.noreply.github.com>
2024-09-29 15:21:51 +0530

    index.js

--- index.js
+++ index.js
@@ -1,44 +1,16 @@
-body{
-    margin: 0;
-    display: flex;
-    justify-content: center;
-    height: 100vh;
-    align-items: center;
-    background-color: darkblue;
-    color: white;
-    font-family: cursive;
-    /* text-align: center; */
+const containerE1=document.querySelector(".container");
 
-}
-.container{
-    display: flex;
-    text-align: center;
-  }
-.mouse-event{
-    border: solid 1px lightgray;
-    margin: 40px;
-    min-width: 180px;
-    min-height: 100px;
-    position: relative;
-    justify-content: center;
-    align-items: center;
-    display: flex;
-    font-size: 30px;
-}
-.mouse-event h4{
-    position: absolute;
-    top: -50px;
-    font-size: 14px;
-    left: 50%;
-    transform: translateX(-50%);
-    white-space: nowrap;
-    font-weight: 100;
-    color: lightgray;
+window.addEventListener("mousemove",(event)=>{
+    containerE1.innerHTML=`
+     <div class="mouse-event">
+          ${event.clientX}
+            <h4>Mouse X Prosition(px)</h4>
+        </div>
+        <div class="mouse-event">
+             ${event.clientY}
+            <h4>Mouse Y Prosition(py)</h4>
+        </div>
+        `
+    
 
-}
-
-@media (max-width:500px){
-    .container{
-        flex-direction: column;
-    }
-}
+})
